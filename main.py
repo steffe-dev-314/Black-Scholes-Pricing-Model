482ef8dcbd0b0b046dc3fe45d6e919e4183e81c5
steffe-dev-314 <283938828+steffe-dev-314@users.noreply.github.com>
2026-05-15 16:47:37 +0200

    removed validation checks in helper functions moved Option and Market to dataclasses removed unneccessary validation checks in Option and Market classes moved edge case check to helper function from get_option_price

--- main.py
+++ main.py
@@ -2,57 +2,52 @@
 import numpy as np 
 from scipy.stats import norm 
 from scipy.optimize import minimize_scalar
+from dataclasses import dataclass
 
 # ------ Helpers ------ 
-
-def _validate_float(x):
-    try:
-        float(x)
-        return float(x)
-    except ValueError:
-        raise ValueError(f"Expected float or int not {type(x)}")
 
 def _calc_PV(r : float ,x : float,tau : float)->float:
     return x*np.exp(-r*tau)
 
 def _get_normal_const(d1 : float,d2 : float) -> tuple[float,float]:
-    d1 = _validate_float(d1)
-    d2 = _validate_float(d2)
+    d1 = float(d1)
+    d2 = float(d2)
     n1 = float(norm.cdf(d1))
     n2 = float(norm.cdf(d2))
     return n1 , n2 
 
 # ------ OptionData ------ 
-
+@dataclass
 class Option:
-    def __init__(self, K : float , T : float , option_type : str , european : bool):
-        K = _validate_float(K)
-        T = _validate_float(T)
-        if K <= 0:
+    K : float 
+    T : float 
+    def __post_init__(self):
+        self.K = float(self.K)
+        self.T = float(self.T)
+        if self.K <= 0:
             raise ValueError("Detected non-positive strike!")
-        if T < 0: # handle T = 0 in calcs ! 
+        if self.T < 0: # handle T = 0 in calcs ! 
             raise ValueError("Detected negative maturity!")
-        self.K = K 
-        self.T = T 
 
 # ------ MarketData ------ 
-
+@dataclass
 class Market:
-    def __init__(self , S : float , sigma : float , r : float):
-        S = _validate_float(S)
-        sigma = _validate_float(sigma)
-        r = _validate_float(r)
 
-        if S < 0: # = 0 special case    
+    S : float 
+    sigma : float 
+    r : float 
+
+    def __post_init__(self):
+        self.S = float(self.S)
+        self.sigma = float(self.sigma)
+        self.r = float(self.r)
+
+        if self.S < 0: # = 0 special case    
             raise ValueError("Detected negative underlying!")
-        if sigma < 0: # = 0 special case 
+        if self.sigma < 0: # = 0 special case 
             raise ValueError("Detected negative volatility!")
-        if r < 0: # = 0 special case
+        if self.r < 0: # = 0 special case
             raise ValueError("Detected negative risk-free rate!")
-        
-        self.S = S 
-        self.sigma = sigma
-        self.r = r 
 
 # ----- Pricing Engine ------        
 
@@ -63,18 +58,15 @@ class Pricing:
     def _get_d1_d2(self,option :Option, market:Market , t:float)->tuple[float,float]:
         
         tau = option.T - t # tau here implicit from option.T and t 
-
-        if tau < 0:
-            raise ValueError(f"Negative time to maturity detected, please make sure t < T, not {t} > {option.T}")
-
-
         sigma = market.sigma
         S = market.S
         r = market.r
         K =option.K
+
         tau_sigma = np.sqrt(tau)*sigma
         d1_fact_1 = tau_sigma**(-1) # tau = 0 --> div by 0 
         d1_fact_2 = np.log(S/K) + tau*(r + sigma**2/2) # S = 0 --> -inf 
+
         d1 = d1_fact_1 * d1_fact_2
         d2 = d1 - tau_sigma
 
@@ -85,17 +77,36 @@ class Pricing:
 
     # ------ Pricing ------
 
-    def get_option_price(self, option :Option, market:Market , t:float)->float:
+    def _check_edge_cases(self,option: Option, market:Market , t :float)->str:
 
         tau = option.T - t
 
         if option.T == 0 or tau == 0: # implies t = tau = 0 due to above check 
-            return self._get_intrinsic_value(option=option,market=market)
+            return 'AT_MATURITY' # self._get_intrinsic_value(option=option,market=market)
         if option.K == 0:
-            return market.S 
+            return 'ZERO_STRIKE' #market.S 
         if market.S == 0:
-            return 0 
+            return 'ZERO_UNDERLYING' # 0
         if market.sigma == 0: # deterministic case --> price = S_0 - exp(-rtau)K
+            return 'DETERMINISTIC' #max(0,market.S - np.exp(-market.r*tau)*option.K)
+        else:
+            return 'USE_FORMULA'
+
+    def get_option_price(self, option :Option, market:Market , t:float)->float:
+
+        tau = option.T - t
+
+        #edge_cases = ['AT_MATURITY' , 'ZERO_STRIKE' , 'ZERO_UNDERLYING' , 'DETERMINISTIC']
+
+        current_case = self._check_edge_cases(option,market,t)
+
+        if current_case == 'AT_MATURITY':
+            return self._get_intrinsic_value(option=option,market=market)
+        if current_case == 'ZERO_STRIKE':
+            return market.S 
+        if current_case == 'ZERO_UNDERLYING':
+            return 0
+        if current_case == 'DETERMINISTIC':
             return max(0,market.S - np.exp(-market.r*tau)*option.K)
 
         d1 , d2 = self._get_d1_d2(option , market , t)
@@ -145,7 +156,7 @@ class Analysis:
     
     def get_rho(self, option :Option , market :Market , model : Pricing , t:float) -> float:
         # del price / del interest 
-        pass 
+        return 0
 
     def get_lambda(self, option : Option , market : Market , model : Pricing , t : float)-> float:
         # underlying/price * Delta 
@@ -162,7 +173,9 @@ class Analysis:
 if __name__ == '__main__':
     # standard setting : S = K = 100 , T = 1 , r = 0.02 , sigma = 0.2 , t = 0, should give P = 8.9 
     market = Market(S= 100, sigma = 0.2, r = 0.02)
-    option = Option(K = 100 , T = 1 , option_type='call' , european=True)
+    #option = Option(K = 100 , T = 1 , option_type='call' , european=True)
+    option = Option(K = 100 , T = 1)
+
     model = Pricing()
     analysis = Analysis()
 
